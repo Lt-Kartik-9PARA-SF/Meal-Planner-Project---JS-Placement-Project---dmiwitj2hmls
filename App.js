969ef1e36214b88a218c1e3c1eb842438dc643aa
Lt-Kartik-9PARA-SF <kartiktyagi22@gmail.com>
2022-12-28 17:04:28 +0530

    some changes

--- App.js
+++ App.js
@@ -1,36 +1,31 @@
 
 let display = document.querySelector('#display1');
 let API_KEY = '7d777b8582d949459baa52f870db9c3a';
-let weight1 = document.querySelector('#weight')
-let height1 = document.querySelector('#height')
-let age1 = document.querySelector('#age')
+let weight1 = document.querySelector('#weight');
+let height1 = document.querySelector('#height');
+let age1 = document.querySelector('#age');
 let DATA = [];
 let minCalories = 200;
 let maxCalories = 1000;
-console.log(display);
 
 
-async function getData() {
 
-    if (height1.value == '' || weight1.value == '' || age1.value == '') {
-        alert("Height Weight and Age Can't be left Blank");
-
-    }
-    else {
-
+async function getData(e) {
+    
 
         console.log('working');
         display1.innerHTML =`<button class="btn btn-primary" type="button" disabled>
         <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
         Loading...
-      </button>`
+      </button>`;
+
         if (height1.value > 175 && weight1.value > 75) {
             minCalories = 1000;
             maxCalories = 3000;
         }
         if (age < 10) {
             maxCalories = 200;
-            minCalories = 10;
+            minCalories = 100;
         }
 
         let apiData = await fetch(`https://api.spoonacular.com/recipes/complexSearch?addRecipeInformation=true&minCalories=${minCalories}&maxCalories=${maxCalories}&number=4&apiKey=${API_KEY}&includeNutrition=true`);
@@ -64,7 +59,7 @@ async function getData() {
 
         display1.innerHTML = html;
 
-    }
+    
 }
  //getData();
 
