3b315346757952c4ddf8e85341983611ba04da1e
Lt-Kartik-9PARA-SF <kartiktyagi22@gmail.com>
2023-01-03 19:42:30 +0530

    removed bugs and added some check points

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
 
 let display = document.querySelector('#display1');
-let API_KEY = '7d777b8582d949459baa52f870db9c3a';
+let API_KEY = '34c8beae57704fdeada45bcc12d0b38c';
 let weight = document.querySelector('#weight');
 let height = document.querySelector('#height');
 let age1 = document.querySelector('#age');
-let DATA = [];
 let minCalories = 200;
-let maxCalories = 0;
+let maxCalories = 1000;
 let btn = document.querySelector('#submit-btn');
 let gender = document.querySelector('#gender');
 let activity = document.querySelector('#activity');
@@ -15,16 +14,17 @@ btn.addEventListener('click', btnClick);
 function btnClick() {
   if (height.value == '' || weight.value == '' || age.value == '' || gender.value == "" || activity.value == "null") {
     alert('All the fields are Mandatory');
-    
+
+
   }
   else {
     getData();
   }
+
 }
 async function getData() {
 
-
-
+  console.log("started");
 
   display1.innerHTML = `<button class="btn btn-primary" type="button" disabled>
         <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
@@ -34,47 +34,63 @@ async function getData() {
   if (gender.value === "male" && activity.value === "light") {
     var BMR = 66.47 + (13.75 * weight.value) + (5.003 * height.value) - (6.755 * age.value);
     maxCalories = BMR * 1.375;
-    console.log('male light')
+    console.log('male light');
   }
 
   else if (gender.value === "male" && activity.value === "moderate") {
     var BMR = 66.47 + (13.75 * weight.value) + (5.003 * height.value) - (6.755 * age.value);
     maxCalories = BMR * 1.55;
+    console.log('male moderate')
   }
 
   else if (gender.value === "male" && activity.value === "active") {
     var BMR = 66.47 + (13.75 * weight.value) + (5.003 * height.value) - (6.755 * age.value);
     maxCalories = BMR * 1.725;
+    console.log('male active')
   }
 
   else if (gender.value === "female" && activity.value === "light") {
     var BMR = 655.1 + (9.563 * weight.value) + (1.850 * height.value) - (4.676 * age.value);
     maxCalories = BMR * 1.375;
+    console.log('female light')
   }
 
   else if (gender.value === "female" && activity.value === "moderate") {
     var BMR = 655.1 + (9.563 * weight.value) + (1.850 * height.value) - (4.676 * age.value);
     maxCalories = BMR * 1.55;
+    console.log('female moderate')
   }
 
   else if (gender.value === "female" && activity.value === "active") {
     var BMR = 655.1 + (9.563 * weight.value) + (1.850 * height.value) - (4.676 * age.value);
     maxCalories = BMR * 1.725;
+    console.log('female active')
   }
 
-  
- 
-  
-  
-  console.log("max calories" + maxCalories);
+  maxCalories = Math.round(maxCalories);
 
-  let apiData = await fetch(`https://api.spoonacular.com/recipes/complexSearch?addRecipeInformation=true&maxCalories=${maxCalories}&number=4&apiKey=${API_KEY}&includeNutrition=true`);
-  let data = await apiData.json();
 
-  code = data.code;
+
+  console.log("max calories => " + maxCalories);
+
+  try {
+
+
+    let apiData = await fetch(`https://api.spoonacular.com/recipes/complexSearch?addRecipeInformation=true&maxCalories=${maxCalories}&number=4&apiKey=${API_KEY}&includeNutrition=true`);
+    var data = await apiData.json();
+    console.log("api data sucessful");
+  }
+
+  catch (e) {
+    console.log("entered catch block");
+    display.innerHTML = `<h1>${e}</h1>`;
+  }
+
 
   data = data.results;
   console.log(data);
+
+
   let html = '';
   data == undefined ? html = `<h1>API limit reached please try again after 24Hrs</h1>` :
 
@@ -103,6 +119,7 @@ async function getData() {
     })
 
   display1.innerHTML = html;
+  console.log(' reached end');
 
 
 }
